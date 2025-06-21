328ad11d0c84882b0d961b033b0a5ccef412cb3c
coperand <coperand199@yandex.ru>
2025-06-21 09:43:11 +0300

    Привёл в соответствие с двенадцатой домашкой

--- src/main.c
+++ src/main.c
@@ -1,10 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <linux/limits.h>
 
 #include "temp_api.h"
 
-int main()
+//Путь к файлу
+char file_path[PATH_MAX];
+//Месяц
+uint8_t month = 0xFF;
+
+
+//Функция разбора опций командной строки
+void parse_args(int argc, char* argv[])
 {
-    struct temperature_on_date_data temperature_on_date1;
+    //Сообщений для вывода через -h или --help
+    char help_str[] =
+    "Options:\n"
+    "-h, --help                          - Print help and exit\n"
+    "-f, --file <path>                   - Set a csv file path\n"
+    "-m, --month <num>                   - Show only one month statistic (0-11)\n";
+
+    //Заполняем параметры опций
+    const struct option long_opts[] =
+    {
+        {"help", no_argument, NULL, 'h'},
+        {"file", required_argument, NULL, 'f'},
+        {"month", required_argument, NULL, 'm'},
+        {NULL, 0, NULL, 0}
+    };
+
+    //Разбираем опции по одной
+    int opt = 0;
+    while( (opt = getopt_long(argc, argv, "hf:m:", long_opts, NULL)) != -1 )
+    {
+        switch (opt)
+        {
+            case 'h':
+                printf("%s", help_str);
+                exit(EXIT_SUCCESS);
+            case 'f':
+                if(optarg)
+                    strncpy(file_path, optarg, PATH_MAX);
+                break;
+            case 'm':
+                if(optarg)
+                    month = atoi(optarg);
+                break;
+            default:
+                printf("Failed to parse argument: %s\n", optarg);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    //Валидация параметров
+    int8_t failure = 0;
+    if(strlen(file_path) == 0)
+    {
+        printf("A file path is not specified\n");
+        failure = 1;
+    }
+    if(month > 11 && month != 0xFF)
+    {
+        printf("A month value is invalid. Must be 0-11\n");
+        failure = 1;
+    }
+    if(failure)
+        exit(EXIT_FAILURE);
+}
+
+int main(int argc, char* argv[])
+{
+    //Инициализируем путь к файлу
+    memset(file_path, 0, sizeof(file_path));
+
+    //Разбираем аргументы командной строки
+    parse_args(argc, argv);
+
+    //DEBUG
+    /* struct temperature_on_date_data temperature_on_date1;
     temperature_on_date1.date.year = 2024;
     temperature_on_date1.date.month = 4;
     temperature_on_date1.date.day = 25;
@@ -26,8 +101,8 @@ int main()
 
     struct temperature_on_date_data temperature_on_date3;
     temperature_on_date3.date.year = 2024;
-    temperature_on_date3.date.month = 4;
-    temperature_on_date3.date.day = 23;
+    temperature_on_date3.date.month = 3;
+    temperature_on_date3.date.day = 25;
     temperature_on_date3.date.hour = 11;
     temperature_on_date3.date.minute = 41;
     temperature_on_date3.temperature = 6;
@@ -47,5 +122,7 @@ int main()
     printf("year_min: %d\n", year_min());
     printf("year_max: %d\n", year_max());
 
-    destroy_list();
+    destroy_list(); */
+
+    //TODO: Реализовать функционал
 }
--- src/temp_api.c
+++ src/temp_api.c
@@ -1,7 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
-
 #include "temp_api.h"
 
 //Головной элемент связного списка
@@ -10,103 +6,323 @@ struct temperatures_list* list_head = NULL;
 
 int8_t date_less(struct date_data date1, struct date_data date2)
 {
-    //TODO: Реализовать
-
-    return -1;
+    return date1.year < date2.year ||
+          (date1.year == date2.year && date1.month < date2.month) ||
+          (date1.year == date2.year && date1.month == date2.month && date1.day < date2.day) ||
+          (date1.year == date2.year && date1.month == date2.month && date1.day == date2.day && date1.hour < date2.hour) ||
+          (date1.year == date2.year && date1.month == date2.month && date1.day == date2.day && date1.hour == date2.hour && date1.minute < date2.minute);
 }
 int8_t date_equal(struct date_data date1, struct date_data date2)
 {
-    //TODO: Реализовать
-
-    return -1;
+    return date1.year == date2.year && date1.month == date2.month && date1.day == date2.day && date1.hour == date2.hour && date1.minute == date2.minute;
 }
 
 
 int8_t is_temperature_on_date_valid(struct temperature_on_date_data temperature_on_date)
 {
-    //TODO: Реализовать
+    //Если какое-либо из полей имеет некорректное значение, возвращаем 0 (аля false)
+    if(temperature_on_date.date.month > 11)
+        return 0;
+    if(temperature_on_date.date.day > 31)
+        return 0;
+    if(temperature_on_date.date.hour > 23)
+        return 0;
+    if(temperature_on_date.date.minute > 59)
+        return 0;
+    if(temperature_on_date.temperature < -99 || temperature_on_date.temperature > 99)
+        return 0;
 
-    return -1;
+    //Если всё ок, возвращаем 1 (аля true)
+    return 1;
 }
 
 
 float month_average(uint8_t month)
 {
-    //TODO: Реализовать
+    //Суммарная температура и количество записей
+    int64_t sum = 0;
+    uint64_t count = 0;
 
-    return -1;
+    //Перебираем все записи и забираем данные из подходящих
+    struct temperatures_list* tmp = list_head;
+    while(tmp != NULL)
+    {
+        if(tmp->temp_on_date.date.month == month)
+        {
+            sum += tmp->temp_on_date.temperature;
+            count++;
+        }
+
+        tmp = tmp->next;
+    }
+
+    //Если ни одного подходящего значения нет, возвращаем NAN
+    if(!count)
+        return NAN;
+
+    //Возвращаем результат, поделив сумму на количество
+    return 1.0 * sum / count;
 }
 
 int8_t month_min(uint8_t month)
 {
-    //TODO: Реализовать
+    //В качестве начального значения берем максимальное для int8_t
+    int8_t min = INT8_MAX;
 
-    return -1;
+    //Перебираем список в поисках меньших значений
+    struct temperatures_list* tmp = list_head;
+    while(tmp != NULL)
+    {
+        if(tmp->temp_on_date.date.month == month && tmp->temp_on_date.temperature < min)
+            min = tmp->temp_on_date.temperature;
+
+        tmp = tmp->next;
+    }
+
+    //Возвращаем найденное минимальное или INT8_MAX
+    return min;
 }
 
 int8_t month_max(uint8_t month)
 {
-    //TODO: Реализовать
+    //В качестве начального значения берем минимальное для int8_t
+    int8_t max = INT8_MIN;
 
-    return -1;
+    //Перебираем список в поисках больших значений
+    struct temperatures_list* tmp = list_head;
+    while(tmp != NULL)
+    {
+        if(tmp->temp_on_date.date.month == month && tmp->temp_on_date.temperature > max)
+            max = tmp->temp_on_date.temperature;
+
+        tmp = tmp->next;
+    }
+
+    //Возвращаем найденное максимальное или INT8_MIN
+    return max;
 }
 
 float year_average()
 {
-    //TODO: Реализовать
+    //Суммарная температура и количество записей
+    int64_t sum = 0;
+    uint64_t count = 0;
 
-    return -1;
+    //Перебираем все записи и забираем данные из подходящих
+    struct temperatures_list* tmp = list_head;
+    while(tmp != NULL)
+    {
+        sum += tmp->temp_on_date.temperature;
+        count++;
+
+        tmp = tmp->next;
+    }
+
+    //Если ни одного подходящего значения нет, возвращаем NAN
+    if(!count)
+        return NAN;
+
+    //Возвращаем результат, поделив сумму на количество
+    return 1.0 * sum / count;
 }
 
 int8_t year_min()
 {
-    //TODO: Реализовать
+    //В качестве начального значения берем максимальное для int8_t
+    int8_t min = INT8_MAX;
 
-    return -1;
+    //Перебираем список в поисках меньших значений
+    struct temperatures_list* tmp = list_head;
+    while(tmp != NULL)
+    {
+        if(tmp->temp_on_date.temperature < min)
+            min = tmp->temp_on_date.temperature;
+
+        tmp = tmp->next;
+    }
+
+    //Возвращаем найденное минимальное или INT8_MAX
+    return min;
 }
 
 int8_t year_max()
 {
-    //TODO: Реализовать
+    //В качестве начального значения берем минимальное для int8_t
+    int8_t max = INT8_MIN;
 
-    return -1;
+    //Перебираем список в поисках больших значений
+    struct temperatures_list* tmp = list_head;
+    while(tmp != NULL)
+    {
+        if(tmp->temp_on_date.temperature > max)
+            max = tmp->temp_on_date.temperature;
+
+        tmp = tmp->next;
+    }
+
+    //Возвращаем найденное максимальное или INT8_MIN
+    return max;
 }
 
 
 void destroy_list()
 {
-    //TODO: Реализовать
+    //Проходим по списку и очищаем память каждого элемента
+    while(list_head != NULL)
+    {
+        struct temperatures_list* tmp = list_head;
+        list_head = tmp->next;
+        free(list_head);
+    }
 }
 
 int8_t push_to_list_front(struct temperature_on_date_data temperature_on_date)
 {
-    //TODO: Реализовать
+    //Валидируем полученные данные
+    if(!is_temperature_on_date_valid(temperature_on_date))
+        return -1;
 
-    return -1;
+    //Выделяем память под новый элемент
+    struct temperatures_list* new_item = malloc(sizeof(struct temperatures_list));
+    if(!new_item)
+        return -2;
+
+    //Интегрируем элемент в список
+    if(list_head)
+        list_head->prev = new_item;
+    new_item->temp_on_date = temperature_on_date;
+    new_item->prev = NULL;
+    new_item->next = list_head;
+
+    //Устанавливаем новый элемент в качестве головного
+    list_head = new_item;
+
+    return 0;
 }
 
 int8_t push_to_list_back(struct temperature_on_date_data temperature_on_date)
 {
-    //TODO: Реализовать
+    //Валидируем полученные данные
+    if(!is_temperature_on_date_valid(temperature_on_date))
+        return -1;
 
-    return -1;
+    //Выделяем память под новый элемент
+    struct temperatures_list* new_item = malloc(sizeof(struct temperatures_list));
+    if(!new_item)
+        return -2;
+
+    //Ищем последний элемент списка, чтобы вставить новый после него
+    struct temperatures_list* last_item = list_head;
+    while(last_item != NULL && last_item->next != NULL)
+        last_item = last_item->next;
+
+    //Интегрируем элемент в список
+    new_item->temp_on_date = temperature_on_date;
+    new_item->prev = last_item;
+    new_item->next = NULL;
+    if(last_item)
+        last_item->next = new_item;
+
+    //Если головного элемента нет, Устанавливаем новый элемент в качестве него
+    if(!list_head)
+        list_head = new_item;
+
+    return 0;
 }
 
 int8_t remove_from_list(struct date_data date)
 {
-    //TODO: Реализовать
+    //Ищем подходящий элемент в списке путём сравнения дат
+    struct temperatures_list* tmp_item = list_head;
+    while(tmp_item != NULL && !date_equal(tmp_item->temp_on_date.date, date))
+        tmp_item = tmp_item->next;
 
-    return -1;
+    //Если не нашли, выходим
+    if(tmp_item == NULL)
+        return -1;
+
+    //Пересматриваем взаимосвязи между элементами списка с учетом планируемого удаления
+    if(tmp_item->prev)
+        tmp_item->prev->next = tmp_item->next;
+    if(tmp_item->next)
+        tmp_item->next->prev = tmp_item->prev;
+    if(tmp_item->prev == NULL)
+        list_head = tmp_item->next;
+
+    //Очищаем память элемента
+    free(tmp_item);
+
+    return 0;
 }
 
 void sort_list()
 {
-    //TODO: Реализовать
+    //Двигаемся от первого элемента списка к предпоследнему
+    struct temperatures_list* tmp_item = list_head;
+    while(tmp_item != NULL && tmp_item->next != NULL)
+    {
+        //Сравнивам текущий элемент со следующим по дате и в случае необходимости вносим изменения
+        if(date_less(tmp_item->next->temp_on_date.date, tmp_item->temp_on_date.date))
+        {
+            //Меняем текущий и следующий элемент местами в списке
+            struct temperatures_list* next_next_item = tmp_item->next->next;
+            tmp_item->next->prev = tmp_item->prev;
+            tmp_item->prev = tmp_item->next;
+            tmp_item->next->next = tmp_item;
+            tmp_item->next = next_next_item;
+
+            //Обновляем головной элемент при необходимости
+            if(list_head == tmp_item)
+                list_head = tmp_item->prev;
+
+            //Сдвигаемся на один элемент назад, чтобы компенсировать смену позиции
+            if(tmp_item->prev)
+                tmp_item = tmp_item->prev;
+
+            //Двигаемся от текущего элемента списка ко второму
+            struct temperatures_list* tmp_item2 = tmp_item;
+            while(tmp_item2 != NULL && tmp_item2->prev != NULL)
+            {
+                //Сравнивам текущий элемент с предыдущим по дате и в случае необходимости вносим изменения
+                if(date_less(tmp_item2->temp_on_date.date, tmp_item2->prev->temp_on_date.date))
+                {
+                    //Меняем текущий и предыдущий элемент местами в списке
+                    struct temperatures_list* prev_prev_item = tmp_item2->prev->prev;
+                    tmp_item2->prev->next = tmp_item2->next;
+                    tmp_item2->next = tmp_item2->prev;
+                    tmp_item2->prev->prev = tmp_item2;
+                    tmp_item2->prev = prev_prev_item;
+
+                    //Обновляем головной элемент при необходимости
+                    if(list_head == tmp_item2->next)
+                        list_head = tmp_item2;
+
+                    //Сдвигаемся на один элемент вперед, чтобы компенсировать смену позиции
+                    if(tmp_item2->next)
+                        tmp_item2 = tmp_item2->next;
+                }
+
+                //Сдвигаемся к предыдущему элементу
+                tmp_item2 = tmp_item2->prev;
+            }
+        }
+
+        //Сдвигаемся к следующему элементу
+        tmp_item = tmp_item->next;
+    }
 }
 
 void print_list()
 {
-    //TODO: Реализовать
+    //Проходимся по списку и пишем в стандартный вывод данные каждого
+    printf("List:\n");
+    struct temperatures_list* tmp_item = list_head;
+    while(tmp_item != NULL)
+    {
+        printf("  %.4u-%.2u-%.2u %.2u:%.2u t: %d\n", tmp_item->temp_on_date.date.year, tmp_item->temp_on_date.date.month, tmp_item->temp_on_date.date.day, tmp_item->temp_on_date.date.hour,
+                                                   tmp_item->temp_on_date.date.minute, tmp_item->temp_on_date.temperature);
+        tmp_item = tmp_item->next;
+    }
 }
 
 
